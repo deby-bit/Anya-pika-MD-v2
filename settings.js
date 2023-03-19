1214b7125ceb41e74d66f06b7a3acdfd49247c94
amdone111 <127435217+amdone111@users.noreply.github.com>
2023-03-19 18:20:19 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,9 +12,9 @@ global.headerText = "Anya by Pika" // text for header of menu
 global.DocumentTitle = "MASTER⚔️PIKA" 
 
 
-global.ownername = "𝕻𝖎𝖐𝖆♰𝕶𝖚𝖓"   // Owner name //        
-global.ownernumber = ['916900904828'] 
-global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_pika" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
+global.ownername = "Midhun,P"   // Owner name //        
+global.ownernumber = ['910539790050'] 
+global.socialm = "𝗜𝗻𝘀𝘁𝗮 : that__null__vector" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
 global.ytname = "YT: Ytname"   // Yt chanel name //
 global.continent = "Asia" // your continent name 
 global.region = "India" // your country name
